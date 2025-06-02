d2a8d2dead5741be35d305ec0e5066c04e6deb99
Rakshit Puri <rakshit.puri@gmail.com>
2025-06-02 19:33:44 +0530

    fix: minor fixes in import

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import StarterKit from "@tiptap/starter-kit";
-import { Link } from "@tiptap/extension-link";
-import { FontFamily } from "@tiptap/extension-font-family";
-import { TextStyle } from "@tiptap/extension-text-style";
-import { Color } from "@tiptap/extension-color";
 import Highlight from "@tiptap/extension-highlight";
 import TaskList from "@tiptap/extension-task-list";
 import TaskItem from "@tiptap/extension-task-item";
 import TableRow from "@tiptap/extension-table-row";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
-import Image from "@tiptap/extension-image";
+import TextAlign from "@tiptap/extension-text-align";
 import ImageResize from "tiptap-extension-resize-image";
-import { useEditorStore } from "@/store/use-editor-store";
-import { ResizableTable } from "@/components/resizable-table";
+import { Link } from "@tiptap/extension-link";
+import { FontFamily } from "@tiptap/extension-font-family";
+import { TextStyle } from "@tiptap/extension-text-style";
+import { Color } from "@tiptap/extension-color";
 import { Underline } from "@tiptap/extension-underline";
 import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditorStore } from "@/store/use-editor-store";
+import { ResizableTable } from "@/components/resizable-table";
 
 export const Editor = () => {
 	const { setEditor } = useEditorStore();
@@ -52,7 +52,6 @@ export const Editor = () => {
 		},
 		extensions: [
 			StarterKit,
-			Image,
 			ImageResize,
 			Link.configure({
 				openOnClick: false,
@@ -60,6 +59,9 @@ export const Editor = () => {
 				defaultProtocol: "https",
 			}),
 			TextStyle,
+			TextAlign.configure({
+				types: ["heading", "paragraph"],
+			}),
 			Color,
 			Underline,
 			FontFamily,
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -33,7 +33,49 @@ import {
 	ImageIcon,
 	UploadIcon,
 	SearchIcon,
+	AlignLeftIcon,
+	AlignCenterIcon,
+	AlignRightIcon,
+	AlignJustifyIcon,
 } from "lucide-react";
+
+const AlignButton = () => {
+	const { editor } = useEditorStore();
+
+	const alignments = [
+		{ label: "Align Left", value: "left", icon: AlignLeftIcon },
+		{ label: "Align Center", value: "center", icon: AlignCenterIcon },
+		{ label: "Align Right", value: "right", icon: AlignRightIcon },
+		{ label: "Justify Content", value: "justify", icon: AlignJustifyIcon },
+	];
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<button className="h-7 min-w-7 shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+					{/* TODO: Change this icon when testAlign updates */}
+					<AlignLeftIcon className="size-4" />
+					<ChevronDownIcon className="size-3 ml-2 shrink-0" />
+				</button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+				{alignments.map(({ label, value, icon: Icon }) => (
+					<DropdownMenuItem
+						key={value}
+						onClick={() => editor?.chain().focus().setTextAlign?.(value).run()}
+						className={cn(
+							"flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+							editor?.isActive({ textAlign: value }) && "bg-neutral-200/80"
+						)}
+					>
+						<Icon className="size-4" />
+						<span className="text-sm">{label}</span>
+					</DropdownMenuItem>
+				))}
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
 
 const ImageButton = () => {
 	const { editor } = useEditorStore();
@@ -385,7 +427,7 @@ export const Toolbar = () => {
 			<Separator orientation="vertical" className="h-6 bg-neutral-300" />
 			<LinkButton />
 			<ImageButton />
-			{/* TODO: Align */}
+			<AlignButton />
 			{/* TODO: Line Height */}
 			{/* TODO: List */}
 			{/* TODO: Highlight Color */}
