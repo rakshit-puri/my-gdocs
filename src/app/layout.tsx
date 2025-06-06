9b9e322693bb927e0b07a85a3c1b769e3846a176
rakshit-puri <rakshit.puri@gmail.com>
2025-06-06 23:19:48 +0530

    feat: setup homepage and navbar search input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
+const inter = Inter({
 	subsets: ["latin"],
 });
 
@@ -27,7 +22,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			<body className={inter.className}>
+				<NuqsAdapter>{children}</NuqsAdapter>
+			</body>
 		</html>
 	);
 }
